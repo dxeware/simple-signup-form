e44fc017bfa6e1a7bfc42ea25f0deedd908b99cd
dxeware <dxeware@gmail.com>
2015-09-21 09:50:22 -0700

    figured out how to test all inputs with jQuery .eq()

--- simple-signup.Spec.js
+++ simple-signup.Spec.js
@@ -17,9 +17,34 @@ describe('optIn', function() {
         scope.$digest();
     }));
     it('should render element and contain form and inputs', function(){
+        var inputEle;
+
+        // Validate form name
         expect(element.find('form').attr('name')).toEqual('myForm');
-        expect(element.find('input').attr('placeholder')).toEqual('first name');
-        expect(element.find('input').attr('type')).toEqual('text');
-        //How to check the other inputs?
+
+        // Validate first name input
+        inputEle = element.find('input').eq(0);
+        expect(inputEle.attr('placeholder')).toEqual('first name');
+        expect(inputEle.attr('type')).toEqual('text');
+        expect(inputEle.attr('required')).toBeTruthy();
+
+        // Validate last name input
+        inputEle = element.find('input').eq(1);
+        expect(inputEle.attr('placeholder')).toEqual('last name');
+        expect(inputEle.attr('type')).toEqual('text');
+        expect(inputEle.attr('required')).toBeTruthy();
+
+        // Validate email input
+        inputEle = element.find('input').eq(2);
+        expect(inputEle.attr('placeholder')).toEqual('email');
+        expect(inputEle.attr('type')).toEqual('text');
+        expect(inputEle.attr('size')).toEqual('48');
+        expect(inputEle.attr('required')).toBeTruthy();
+
+        // Validate submit input
+        inputEle = element.find('input').eq(3);
+        expect(inputEle.attr('value')).toEqual('Give us ur info!');
+        expect(inputEle.attr('type')).toEqual('submit');
+        expect(inputEle.attr('size')).toEqual('48');
     });
 });
